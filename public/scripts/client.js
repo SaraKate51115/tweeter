f28749add68e1ea256275eacc2bfaaffd441e411
SaraKate51115 <git@github.com:SaraKate51115/tinyapp.git>
2020-11-19 21:25:59 +0000

    created tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,32 +8,71 @@
 $(document).ready(() => {
 
     // Test / driver code (temporary). Eventually will get this from the server.
-    const tweetData = {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
+    const data = [
+      {
+        "user": {
+          "name": "Newton",
+          "avatars": "https://i.imgur.com/73hZDYK.png"
+          ,
           "handle": "@SirIsaac"
-    },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "content": {
+          "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
       },
-      "created_at": 1461116232227
-    }
-    //=> TURN INTO MARKUP???
+      {
+        "user": {
+          "name": "Descartes",
+          "avatars": "https://i.imgur.com/nlhLi3I.png",
+          "handle": "@rd" },
+        "content": {
+          "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+      }
+    ]
+   
 
   //Function Implementation:
   const createTweetElement = function(tweetData) {
     //return <article> element containing the enitre HTML structure of the tweet
-    const $tweet = $(`<article class="tweet"></article>`); //=> creates hard coded tweets
+    const $tweet = $(`<article class="tweet">Hello</article>`); //=> creates hard coded tweets
+
+    //REVIEW: IS BELOW CORRECT???
+    const markup = `
+    <article class="tweet">
+    <div class="header">
+      <div>
+        <img src="${tweetData.user.avatars}">
+        <h2>${tweetData.user.name}</h2>
+      </div>
+      <h3>${tweetData.user.handle}</h3>
+    </div>
+    <div class="content">
+      <p>${tweetData.content.text}</p>
+    </div>
+    <hr>
+    <footer>
+      <p>${moment(tweetData.created_at).fromNow()}</p>
+      <div class="icons">&#9873 &#10226 &#9829</div>
+    </footer> 
+</article>  
+      `;
+
+      //document.createElement?
+
     console.log(`${tweetData}`)
-    return $tweet;
+    // return $tweet;
+    return markup;
   };
 
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements,
-
+  const renderTweet = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  }
+  renderTweet(data);
 });
+
